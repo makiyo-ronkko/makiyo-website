5e4971df0587b302fe3038dd5c60b62831a8075d
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-09-21 17:01:49 +0300

    Fix img path

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -22,7 +22,7 @@ class Home extends Component {
             <p>{this.props.text2}</p>
           </div>
           <div className='about-image'>
-            <img src='/makiyo.png' alt={this.props.selfie} />
+            <img src='./makiyo.png' alt={this.props.selfie} />
           </div>
         </div>
       </div>
