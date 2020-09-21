374b9f2806d7a3986304f8f64926c87f861e8c1e
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-09-21 18:35:06 +0300

    Update className

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,19 +2,8 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div className='section contact' id='contact'>
-      <div id='mycv-container'>
-        <div id='mycv'>
-          <h2>My CV here!</h2>
-          <div id='pdf-btn'>
-            <button onClick={() => this.openCV()}>
-              {' '}
-              <i className='fas fa-download fa-2x'></i>
-            </button>
-          </div>
-        </div>
-      </div>
-      <div id='mycontact-container'>
+    <div className='Contact section' id='contact'>
+      <div className='mycontact-container'>
         <div id='mycontact'>
           <h2>Feel free to contact me :)</h2>
           <div className='email-btn'>
