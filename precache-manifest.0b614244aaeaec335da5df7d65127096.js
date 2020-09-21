a90d5f302394de061d5d5085cb435d39cad4cb78
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-09-21 16:31:49 +0300

    Updates

---
+++ precache-manifest.0b614244aaeaec335da5df7d65127096.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a241ccff2e6486a00840106d9b0cd98d",
+    "url": "/makiyo-website/index.html"
+  },
+  {
+    "revision": "75db3d54496802f0bd8c",
+    "url": "/makiyo-website/static/css/main.eb772ecf.chunk.css"
+  },
+  {
+    "revision": "9649576d801d86191c19",
+    "url": "/makiyo-website/static/js/2.cefd8283.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/makiyo-website/static/js/2.cefd8283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75db3d54496802f0bd8c",
+    "url": "/makiyo-website/static/js/main.b4f1d60f.chunk.js"
+  },
+  {
+    "revision": "5dba69579feb0c101b32",
+    "url": "/makiyo-website/static/js/runtime-main.77f2ee5b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/makiyo-ronkko/makiyo-website/precache-manifest.e0e1cc02ff3c867d6cf8dc901c215e9c.js"
+  "/makiyo-website/precache-manifest.0b614244aaeaec335da5df7d65127096.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/makiyo-ronkko/makiyo-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/makiyo-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
