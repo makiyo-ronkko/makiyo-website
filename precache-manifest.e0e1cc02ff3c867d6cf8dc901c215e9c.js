eb3ed2ca6fef7fb8c399f95f31377b8b413b13a2
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-09-21 16:26:31 +0300

    Updates

---
+++ precache-manifest.e0e1cc02ff3c867d6cf8dc901c215e9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94774a5e18788188beb7509b947a77a4",
+    "url": "/makiyo-ronkko/makiyo-website/index.html"
+  },
+  {
+    "revision": "75db3d54496802f0bd8c",
+    "url": "/makiyo-ronkko/makiyo-website/static/css/main.eb772ecf.chunk.css"
+  },
+  {
+    "revision": "9649576d801d86191c19",
+    "url": "/makiyo-ronkko/makiyo-website/static/js/2.cefd8283.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/makiyo-ronkko/makiyo-website/static/js/2.cefd8283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75db3d54496802f0bd8c",
+    "url": "/makiyo-ronkko/makiyo-website/static/js/main.b4f1d60f.chunk.js"
+  },
+  {
+    "revision": "6fe6c7d5105b6732dae6",
+    "url": "/makiyo-ronkko/makiyo-website/static/js/runtime-main.880be146.js"
+  }
+]);
