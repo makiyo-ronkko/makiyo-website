63a1a17231df9acb7f02578ceccea627eb2478fe
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-10-25 22:38:27 +0200

    Update project details

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,59 +1,42 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
+import project from '../../img/project.png';
 
-class Project extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      imgUrl: '',
-      login: '',
-      isLoaded: false,
-    };
-  }
-
-  async componentDidMount() {
-    const url = `https://api.github.com/users/${this.props.username}`;
-    let response = await axios.get(url);
-    let data = response.data;
-    console.log(data);
-    this.setState({
-      imgUrl: data.avatar_url,
-      login: data.login,
-      isLoaded: true,
-    });
-  }
-
-  render() {
-    return (
-      <div className='Project section' id='project'>
-        <div className='Project-container'>
-          <div className='Proejct-title'>
-            <h1 style={{ margin: '20px', textTransform: 'uppercase' }}>
-              Project
-            </h1>
-            {/* <img
-              src={this.state.imgUrl}
-              alt='makiyo-ronkko'
-              style={{ width: '10%' }}
-            /> */}
-          </div>
-          <h3 style={{ margin: '20px' }}>
-            My Github account: {this.state.login}
-          </h3>
-          <div style={{ textAlign: 'center' }}>
-            <p>♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢</p>
-            <p>♢♢♢♢♢ Under construction ♢♢♢♢♢</p>
-            <p>♢♢♢♢♢♢♢ Coming soon! ♢♢♢♢♢♢♢</p>
-            <p>♢♢♢♢ Thank you for your patience! ♢♢♢♢</p>
-            <p>♢♢ In the meantime, please visit my github ♢♢</p>
-            <p>♢♢♢♢♢♢♢♢ Kiitos paljon! ♢♢♢♢♢♢♢♢</p>
-            <p>♢♢♢♢♢♢♢ ありがとうございます! ♢♢♢♢♢♢♢</p>
-            <p>♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢♢</p>
-          </div>
-        </div>
+const Project = () => {
+  return (
+    <div
+      className='section Project box'
+      id='project'
+      style={{ flexDirection: 'column', marginTop: '32px', background: 'none' }}
+    >
+      <div className='Proejct-title'>
+        <h1
+          style={{
+            margin: '20px',
+            textTransform: 'uppercase',
+            textDecoration: 'underline',
+          }}
+        >
+          Project
+        </h1>
       </div>
-    );
-  }
-}
+      <button className='cardBtn' style={{ margin: '16px 0' }}>
+        <a
+          style={{
+            textDecoration: 'none',
+            color: '#000000',
+          }}
+          href='//github.com/makiyo-ronkko'
+          target='_blank'
+        >
+          My Github Page
+        </a>
+      </button>
+      <p>♢ Coming soon! ♢</p>
+      <h3>Full Stack Social Media App</h3>
+      <h3>Daily Art Sharing</h3>
+      <img src={project} alt='makiyo-ronkko' style={{ width: '30%' }} />
+    </div>
+  );
+};
 
 export default Project;
