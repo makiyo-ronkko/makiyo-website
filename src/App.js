6e217d4bf8b60b15d17c4c2d98fb768ea48f44c6
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-10-31 14:27:12 +0200

    Remove unnecessary codes

--- src/App.js
+++ src/App.js
@@ -8,36 +8,21 @@ import Contact from './Components/Contact/Contact';
 import Footer from './Components/Footer/Footer';
 
 class App extends Component {
-  /* Scroll top button */
-
-  // scroll = () => {
-  //   const pagetop = '#page_top';
-  //   pagetop.hide();
-  //   window.scroll(function () {
-  //     if (scrollTop() > 100) {
-  //       //appear after 100px scroll down
-  //       pagetop.fadeIn();
-  //     } else {
-  //       pagetop.fadeOut();
-  //     }
-  //   });
-  // };
-
-  render() {
-    return (
-      <div className='wrapper'>
-        <div id='page_top'>
-          <a href='#top'></a>
-        </div>
-        <Nav />
-        <Home />
-        <Card />
-        <Project username='makiyo-ronkko' />
-        <Contact />
-        <Footer />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='wrapper'>
+				<div id='page_top'>
+					<a href='#top'></a>
+				</div>
+				<Nav />
+				<Home />
+				<Card />
+				<Project username='makiyo-ronkko' />
+				<Contact />
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default App;
