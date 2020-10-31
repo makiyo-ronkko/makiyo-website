5ab8768cecd40f7bb9dfafe64a3090e75f1b156a
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-10-31 14:27:51 +0200

    Update styling

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
 const Contact = () => {
-  return (
-    <div className='Contact section' id='contact'>
-      <div className='mycontact-container'>
-        <div id='mycontact'>
-          <h2>Feel free to contact me :)</h2>
-          <div className='email-btn'>
-            <div className='icon'>
-              <a href='mailto:makiyo.ronkko@bc.edu.fi'>
-                <i className='far fa-envelope fa-2x'></i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='Contact section' id='contact' style={{ height: '100px' }}>
+			<div className='mycontact-container'>
+				<div id='mycontact'>
+					<h2>Feel free to contact me :)</h2>
+					<div className='email-btn'>
+						<div className='icon'>
+							<a href='mailto:makiyo.ronkko@bc.edu.fi'>
+								<i className='far fa-envelope fa-2x'></i>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Contact;
