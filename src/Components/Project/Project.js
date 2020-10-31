dc201f32c7393bb85c229b3e80a7c353d688d60c
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-10-31 14:27:39 +0200

    Update to class component

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,42 +1,106 @@
-import React from 'react';
+import React, { Component } from 'react';
 import project from '../../img/project.png';
+import './Project.css';
 
-const Project = () => {
-  return (
-    <div
-      className='section Project box'
-      id='project'
-      style={{ flexDirection: 'column', marginTop: '32px', background: 'none' }}
-    >
-      <div className='Proejct-title'>
-        <h1
-          style={{
-            margin: '20px',
-            textTransform: 'uppercase',
-            textDecoration: 'underline',
-          }}
-        >
-          Project
-        </h1>
-      </div>
-      <button className='cardBtn' style={{ margin: '16px 0' }}>
-        <a
-          style={{
-            textDecoration: 'none',
-            color: '#000000',
-          }}
-          href='//github.com/makiyo-ronkko'
-          target='_blank'
-        >
-          My Github Page
-        </a>
-      </button>
-      <p>♢ Coming soon! ♢</p>
-      <h3>Full Stack Social Media App</h3>
-      <h3>Daily Art Sharing</h3>
-      <img src={project} alt='makiyo-ronkko' style={{ width: '30%' }} />
-    </div>
-  );
-};
+class Project extends Component {
+	static defaultProps = {
+		appName: 'https://dailyartsharing.herokuapp.com/ ',
+		githubLink: 'https://github.com/makiyo-ronkko/fullstack-app',
+		about:
+			'I have built various apps for my own projects and school. Here is the latest fullstack project I would like to present.',
+		title: '♢ Daily Art Sharing ♢',
+		appDesc:
+			"'Daily Art Sharing' is a social media app where people can share arts and pictures daily and get ideas and inspiration from peer artists.",
+		appDesc2:
+			'App is created with Backend: Mongo DB, Express and Frontend: React, Redux. Design with CSS. Visual assets with Adobe Spark.',
+		appDesc3: 'Main functionalities: ',
+		appDesc4: 'CRUD (Registration, Login, Logout, Post, Comment)',
+	};
+	render() {
+		return (
+			<div
+				className='Project-section Project box'
+				id='project'
+				style={{ background: 'none' }}
+			>
+				<div
+					className='Proejct-title'
+					style={{ textDecoration: 'uppercase', margin: '20px' }}
+				>
+					<h1 style={{ textTransform: 'uppercase' }}>Project</h1>
+				</div>
+				<h4>{this.props.about}</h4>
+
+				<div className='section'>
+					<div className='Project-box'>
+						<h4>
+							<a
+								href='https://dailyartsharing.herokuapp.com'
+								target='_blank'
+								style={{
+									textDecoration: 'none',
+									backgroundColor: '#f4d6db',
+									color: 'black',
+								}}
+							>
+								{this.props.appName}
+							</a>
+						</h4>
+						<br />
+						<h4>
+							<a
+								href='https://github.com/makiyo-ronkko/fullstack-app'
+								target='_blank'
+								style={{
+									textDecoration: 'none',
+									backgroundColor: '#f4d6db',
+									color: 'black',
+								}}
+							>
+								{this.props.githubLink}
+							</a>
+						</h4>
+						<p style={{ margin: '0.2rem', padding: '0.2rem' }}>
+							{this.props.title}
+						</p>
+						<a
+							href='https://github.com/makiyo-ronkko/fullstack-app'
+							target='_blank'
+						>
+							<img
+								src={project}
+								alt='makiyo-ronkko'
+								className='Project-img'
+								style={{ width: '30%' }}
+							/>
+						</a>
+					</div>
+				</div>
+
+				<div className='description-box'>
+					<p>{this.props.appDesc} </p>
+					<p>{this.props.appDesc2}</p>
+					<p>
+						{this.props.appDesc3}
+						{this.props.appDesc4}
+					</p>
+				</div>
+
+				<button className='projectBtn' style={{ margin: '16px 0' }}>
+					<a
+						style={{
+							textDecoration: 'none',
+							color: '#000000',
+						}}
+						href='//github.com/makiyo-ronkko'
+						target='_blank'
+					>
+						My Github Page
+					</a>
+				</button>
+			</div>
+		);
+	}
+}
 
 export default Project;
