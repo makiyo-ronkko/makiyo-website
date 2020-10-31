f72fadc6c575d484812dd7327c5a5c19a3807a5e
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-10-31 09:06:12 +0200

    Update texts

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,35 +2,35 @@ import React, { Component } from 'react';
 import './Home.css';
 
 class Home extends Component {
-  static defaultProps = {
-    selfie: 'selfie',
-    title: 'Hello, my name is Makiyo.',
-    subtitle: "I'm a junior web developer.",
-    text:
-      'In 2019, I made a decision to change my career and commitment to be a software developer.',
-    text2:
-      'Born in Japan living in Finland. Former international sales and marketing professional enoys learning and developing programming skills.',
-  };
-  render() {
-    return (
-      <div className='Hero-banner section background-animated' id='top'>
-        <div id='about'>
-          <div className='about-text'>
-            <h1>{this.props.title}</h1>
-            <p>{this.props.subtitle}</p>
-            <p>{this.props.text}</p>
-            <p>{this.props.text2}</p>
-          </div>
-          <div className='about-image'>
-            <img
-              src={process.env.PUBLIC_URL + '/makiyo.png'}
-              alt={this.props.selfie}
-            />
-          </div>
-        </div>
-      </div>
-    );
-  }
+	static defaultProps = {
+		selfie: 'selfie',
+		title: 'Hello, my name is Makiyo.',
+		subtitle: "I'm a junior web developer.",
+		text:
+			'In 2019, I made a decision to change my career and commitment to be a software developer.',
+		text2:
+			'Born in Japan living in Finland. Former international sales and marketing professional enjoy learning and developing programming skills.',
+	};
+	render() {
+		return (
+			<div className='Hero-banner section background-animated' id='top'>
+				<div id='about'>
+					<div className='about-text'>
+						<h1>{this.props.title}</h1>
+						<p>{this.props.subtitle}</p>
+						<p>{this.props.text}</p>
+						<p>{this.props.text2}</p>
+					</div>
+					<div className='about-image'>
+						<img
+							src={process.env.PUBLIC_URL + '/makiyo.png'}
+							alt={this.props.selfie}
+						/>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Home;
