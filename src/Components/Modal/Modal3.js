4fb7b2a5a918f519273b7c1dca177a199e00838d
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-11-01 22:45:17 +0200

    Add Linkedin icon and link

--- src/Components/Modal/Modal3.js
+++ src/Components/Modal/Modal3.js
@@ -8,7 +8,7 @@ const Modal3 = ({ handleClose3, show3 }) => {
 
 	return (
 		<div id='education-pop-up' className='Modal' style={{ display: 'block' }}>
-			<div className='Modal-content'>
+			<div className='Modal-content' id='education-modal'>
 				<span className='close' onClick={handleClose3}>
 					&times;
 				</span>
@@ -27,7 +27,16 @@ const Modal3 = ({ handleClose3, show3 }) => {
 				</p>
 				<h2>01/2003- 12/2005: Los Angeles City College, Los Angeles, U.S.A.</h2>
 				<p>Associate of Art in Music (AA)</p>
-				<h2>For details, please visit my Linkedin page.</h2>
+				<div>
+					<h2>For details, please visit my Linkedin page.</h2>
+					<a
+						href='//linkedin.com/in/makiyoronkko '
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						<i className='fab fa-linkedin fa-2x'></i>
+					</a>
+				</div>
 			</div>
 		</div>
 	);
