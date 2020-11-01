84168d03b94efd12a6027d02f21eb2d595662f0f
makiyo-ronkko <makiyo.ronkko@gmail.com>
2020-11-01 22:46:32 +0200

    Remove unnecessary codes

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -6,106 +6,110 @@ import Modal2 from '../Modal/Modal2';
 import Modal3 from '../Modal/Modal3';
 
 class Card extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      show1: false,
-      show2: false,
-      show3: false,
-    };
+		this.state = {
+			show1: false,
+			show2: false,
+			show3: false,
+		};
 
-    //this.toggleNav = this.toggleNav.bind(this);
-    this.showModal1 = this.showModal1.bind(this);
-    this.showModal2 = this.showModal2.bind(this);
-    this.showModal3 = this.showModal3.bind(this);
+		this.showModal1 = this.showModal1.bind(this);
+		this.showModal2 = this.showModal2.bind(this);
+		this.showModal3 = this.showModal3.bind(this);
 
-    this.hideModal1 = this.hideModal1.bind(this);
-    this.hideModal2 = this.hideModal2.bind(this);
-    this.hideModal3 = this.hideModal3.bind(this);
-  }
+		this.hideModal1 = this.hideModal1.bind(this);
+		this.hideModal2 = this.hideModal2.bind(this);
+		this.hideModal3 = this.hideModal3.bind(this);
+	}
 
-  showModal1 = () => {
-    this.setState({
-      show1: true,
-    });
-  };
-  showModal2 = () => {
-    this.setState({
-      show2: true,
-    });
-  };
-  showModal3 = () => {
-    this.setState({
-      show3: true,
-    });
-  };
+	showModal1 = () => {
+		this.setState({
+			show1: true,
+		});
+	};
 
-  hideModal1 = () => {
-    this.setState({
-      show1: false,
-    });
-  };
-  hideModal2 = () => {
-    this.setState({
-      show2: false,
-    });
-  };
-  hideModal3 = () => {
-    this.setState({
-      show3: false,
-    });
-  };
-  render() {
-    return (
-      <>
-        <div className='Card-1 section'>
-          <div className='box' id='skills'>
-            <div className='Card-container'>
-              <button
-                id='skillsBtn'
-                className='cardBtn'
-                onClick={this.showModal1}
-              >
-                Skills
-              </button>
-            </div>
-          </div>
-          <Modal show1={this.state.show1} handleClose1={this.hideModal1} />
-        </div>
+	showModal2 = () => {
+		this.setState({
+			show2: true,
+		});
+	};
 
-        <div className='Card-2 section'>
-          <div className='box' id='workexperience'>
-            <div className='Card-container'>
-              <button
-                id='workBtn'
-                className='cardBtn'
-                onClick={this.showModal2}
-              >
-                Work Experience
-              </button>
-            </div>
-          </div>
-          <Modal2 show2={this.state.show2} handleClose2={this.hideModal2} />
-        </div>
+	showModal3 = () => {
+		this.setState({
+			show3: true,
+		});
+	};
 
-        <div className='Card-3 section'>
-          <div className='box' id='education'>
-            <div className='Card-container'>
-              <button
-                id='educationBtn'
-                className='cardBtn'
-                onClick={this.showModal3}
-              >
-                Education
-              </button>
-            </div>
-          </div>
-          <Modal3 show3={this.state.show3} handleClose3={this.hideModal3} />
-        </div>
-      </>
-    );
-  }
+	hideModal1 = () => {
+		this.setState({
+			show1: false,
+		});
+	};
+
+	hideModal2 = () => {
+		this.setState({
+			show2: false,
+		});
+	};
+
+	hideModal3 = () => {
+		this.setState({
+			show3: false,
+		});
+	};
+
+	render() {
+		return (
+			<>
+				<div className='Card-1 section'>
+					<div className='box' id='skills'>
+						<div className='Card-container'>
+							<button
+								id='skillsBtn'
+								className='cardBtn'
+								onClick={this.showModal1}
+							>
+								Skills
+							</button>
+						</div>
+					</div>
+					<Modal show1={this.state.show1} handleClose1={this.hideModal1} />
+				</div>
+
+				<div className='Card-2 section'>
+					<div className='box' id='workexperience'>
+						<div className='Card-container'>
+							<button
+								id='workBtn'
+								className='cardBtn'
+								onClick={this.showModal2}
+							>
+								Work Experience
+							</button>
+						</div>
+					</div>
+					<Modal2 show2={this.state.show2} handleClose2={this.hideModal2} />
+				</div>
+
+				<div className='Card-3 section'>
+					<div className='box' id='education'>
+						<div className='Card-container'>
+							<button
+								id='educationBtn'
+								className='cardBtn'
+								onClick={this.showModal3}
+							>
+								Education
+							</button>
+						</div>
+					</div>
+					<Modal3 show3={this.state.show3} handleClose3={this.hideModal3} />
+				</div>
+			</>
+		);
+	}
 }
 
 export default Card;
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -2,34 +2,30 @@ import React, { Component } from 'react';
 import './Nav.css';
 
 class Nav extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      toggle: false,
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			toggle: false,
+		};
+	}
 
-  render() {
-    return (
-      <header>
-        <ul className='Nav'>
-          <li className='navlink'>
-            <a href='#top'>Home</a>
-          </li>
-          <li className='navlink'>
-            <a href='#project'>Projects</a>
-          </li>
-          <li className='navlink'>
-            <a href='#contact'>Contact</a>
-          </li>
-        </ul>
-        {/* <div className='burger'>
-          <i className='fas fa-bars'></i>
-          <i onClick={this.toggleNav()} className='fas fa-bars'></i> 
-        </div> */}
-      </header>
-    );
-  }
+	render() {
+		return (
+			<header>
+				<ul className='Nav'>
+					<li className='navlink'>
+						<a href='#top'>Home</a>
+					</li>
+					<li className='navlink'>
+						<a href='#project'>Projects</a>
+					</li>
+					<li className='navlink'>
+						<a href='#contact'>Contact</a>
+					</li>
+				</ul>
+			</header>
+		);
+	}
 }
 
 export default Nav;
